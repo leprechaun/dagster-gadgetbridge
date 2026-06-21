a04831e52fe5a42eb2d7ba82afbe01c1e95de6a1
leprechaun <leprechaun@gmail.com>
2026-06-21 23:58:24 +0700

    add heart rate quantiles

--- src/gadgetbridge_pipeline/defs/assets/gold.py
+++ src/gadgetbridge_pipeline/defs/assets/gold.py
@@ -8,6 +8,7 @@ from dagster import AutomationCondition, Definitions, AssetExecutionContext
     io_manager_key="gadgetbridge_gold_io_manager",
     key_prefix="gadgetbridge-gold",
     ins={
+        "activity_sample":         dg.AssetIn(key=dg.AssetKey(["gadgetbridge-silver", "huami_extended_activity_sample"])),
         "hrv":         dg.AssetIn(key=dg.AssetKey(["gadgetbridge-silver", "generic_hrv_value_sample"])),
         "spo2":        dg.AssetIn(key=dg.AssetKey(["gadgetbridge-silver", "huami_spo2_sample"])),
         "stress":      dg.AssetIn(key=dg.AssetKey(["gadgetbridge-silver", "huami_stress_sample"])),
@@ -16,6 +17,7 @@ from dagster import AutomationCondition, Definitions, AssetExecutionContext
     automation_condition=AutomationCondition.eager(),
 )
 def daily_health_snapshot(
+    activity_sample: pl.DataFrame,
     hrv: pl.DataFrame,
     spo2: pl.DataFrame,
     stress: pl.DataFrame,
@@ -35,6 +37,20 @@ def daily_health_snapshot(
         by_day(temperature, "TEMPERATURE", "avg_temperature_c"),
     ]
 
+    frames.append(
+        activity_sample.with_columns(
+            pl.col("TIMESTAMP").dt.date().alias("date")
+        ).group_by(
+            ["date"]
+        ).agg(
+            pl.col("HEART_RATE").min().alias("heart_rate_min"),
+            pl.col("HEART_RATE").max().alias("heart_rate_max"),
+            pl.col("HEART_RATE").quantile(0.5).alias("heart_rate_median"),
+            pl.col("HEART_RATE").quantile(0.1).alias("heart_rate_p10"),
+            pl.col("HEART_RATE").quantile(0.9).alias("heart_rate_p90"),
+        )
+    )
+
     result = frames[0]
 
     for f in frames[1:]:
@@ -43,6 +59,5 @@ def daily_health_snapshot(
     sorted_df = result.sort("date")
 
     return sorted_df
-
 
 defs = Definitions(assets=[daily_health_snapshot])
