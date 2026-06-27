21f22773c986f4d6473794b829c6545fb29a28c0
leprechaun <leprechaun@gmail.com>
2026-06-27 17:54:43 +0700

    add description on all bronze assets

--- src/gadgetbridge_pipeline/defs/assets/bronze.py
+++ src/gadgetbridge_pipeline/defs/assets/bronze.py
@@ -19,31 +19,40 @@ def _read_table(table: str, db_path: str) -> pl.DataFrame:
 
 _TABLES = {
     "huami_extended_activity_sample": {
-        "epoch_unit": "s"
+        "epoch_unit": "s",
+        "description": "a wide table of per minute metrics including step count, sleep, vigor of movement, etc"
     },
     "generic_temperature_sample": {
-        "epoch_unit": "ms"
+        "epoch_unit": "ms",
+        "description": "temperature of the sensor"
     },
     "huami_sleep_respiratory_rate_sample": {
-        "epoch_unit": "ms"
+        "epoch_unit": "ms",
+        "description": "night time respiratory rate"
     },
     "generic_hrv_value_sample": {
-        "epoch_unit": "ms"
+        "epoch_unit": "ms",
+        "description": "Heart rate variability"
     },
     "huami_stress_sample": {
-        "epoch_unit": "ms"
+        "epoch_unit": "ms",
+        "description": "A bad nmeasurement of stress, based on HRV. Inaccurate."
     },
     "huami_spo2_sample": {
-        "epoch_unit": "ms"
+        "epoch_unit": "ms",
+        "description": "SPO2 samples: more often at night"
     },
     "huami_pai_sample": {
-        "epoch_unit": "ms"
+        "epoch_unit": "ms",
+        "description": "Amazfit's calculation of the PAI health metric"
     },
     "battery_level": {
-        "epoch_unit": "s"
+        "epoch_unit": "s",
+        "description": "The battery level over time: 0% to 100%"
     },
     "huami_sleep_session_sample": {
-        "epoch_unit": "ms"
+        "epoch_unit": "ms",
+        "description": "sleep session with binary data. there can be overlapping sessions during the same day."
     },
 }
 
@@ -56,6 +65,7 @@ def _make_bronze_asset(table_name: str, settings: Dict[str, str]):
         io_manager_key="deltalake_io_manager",
         key_prefix=["gadgetbridge", "bronze"],
         automation_condition=AutomationCondition.eager(),
+        description=settings.get('description')
     )
     def _asset(context: AssetExecutionContext, gadgetbridge_db_file) -> pl.DataFrame:
         return apply_bronze_transform(
